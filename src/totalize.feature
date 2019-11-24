59202bf21cbcf4d62eb4cde60490dfbfbcb3b3bc
Neil Crawford <neil.crawford@gmail.com>
2019-11-24 21:59:08 +0000

    Adding cucumber-js as test harness

---
+++ src/totalize.feature
@@ -0,0 +1,8 @@
+Feature: First feature
+
+  My first feature.
+
+  Scenario: First scenario
+    Given a first given step
+    When I run cucumber-js
+    Then it fails
--- src/totalize.spec.js
+++ src/totalize.spec.js
@@ -1,10 +1,25 @@
-const totalize = require("./totalize");
+// const totalize = require("./totalize");
+const { Given, When, Then } = require("cucumber");
+// describe("Totalize tests", () => {
+//   describe("Kids Easy Mode", () => {
+//     it("First test", () => {
+//       const result = totalize();
+//       expect(result).toBeUndefined();
+//     });
+//   });
+// });
 
-describe("Totalize tests", () => {
-  describe("Kids Easy Mode", () => {
-    it("First test", () => {
-      const result = totalize();
-      expect(result).toBeUndefined();
-    });
-  });
+Given("a first given step", function() {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("I run cucumber-js", function() {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("it fails", function() {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
 });
